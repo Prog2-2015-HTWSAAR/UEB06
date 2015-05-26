4f0e9395399447b1cba32890b4112f9a657307b3
Simon Bastian <s9sibast@stud.uni-saarland.de>
2015-05-26 20:28:51 +0200

    basic implementation

--- UEB5/Lager.cpp
+++ UEB5/Lager.cpp
@@ -11,35 +11,64 @@ Lager::Lager(int maxAnzArtikel) {
 	this->anzArtikel = 0;
 	this->maxAnzArtikel = maxAnzArtikel;
 	this->name = "Musterlager";
-	this->sortiment = new Artikel*[maxAnzArtikel];
+	this->artikelTab = new Artikel*[maxAnzArtikel];
 }
 Lager::Lager(int maxAnzArtikel, string name){
 	this->anzArtikel = 0;
 	this->maxAnzArtikel = maxAnzArtikel;
 	this->name = name;
-	this->sortiment = new Artikel*[maxAnzArtikel];
+	this->artikelTab = new Artikel*[maxAnzArtikel];
 }
 Lager::~Lager() {
-	// TODO Auto-generated destructor stub
+	loescheAlleArtikel();
 }
 
 void Lager::createArtikel(int artikelNr, string bezeichnung, double artikelPreis){
-	Artikel* ap = new Artikel(artikelNr,bezeichnung,artikelPreis);
-	sortiment[anzArtikel++]=ap;
+	Artikel* ap = new Artikel(artikelNr, bezeichnung, artikelPreis);
+	artikelTab[anzArtikel] = ap;
+	anzArtikel++;
 }
 void Lager::createArtikel(int artikelNr, string bezeichnung, double artikelPreis, int bestand){
 	Artikel* ap = new Artikel(artikelNr,bezeichnung,artikelPreis,bestand);
-	sortiment[anzArtikel++]=ap;
+	artikelTab[anzArtikel] = ap;
+	anzArtikel++;
 }
 void Lager::deleteArtikel(int artikelNr){
 	int i = findeArtikel(artikelNr);
 	if(i >= 0){
-		delete sortiment[i];
+		delete artikelTab[i];
 		// letztes Konto an die Stelle i setzen
 		if(i != anzArtikel-1){
-			sortiment[i] = sortiment[anzArtikel-1];
+			artikelTab[i] = artikelTab[anzArtikel-1];
 		}
-		sortiment[--anzArtikel] = 0;
+		artikelTab[anzArtikel-1] = 0;
+		anzArtikel--;
 	}
+}
+void Lager::bucheAbgang(Artikel* artikel, int menge){
+	artikel->bucheAbgang(menge);
+}
+
+void Lager::bucheZugang(Artikel* artikel, int menge){
+	artikel->bucheZugang(menge);
+}
+void Lager::preiseAendern(double preisAenderung){
+	for(int i = 0; i < anzArtikel; i++){
+		artikelTab[i]->aenderePreis(preisAenderung);
+	}
+}
+int Lager::findeArtikel(int artikelNr){
+	for(int i = 0; i < anzArtikel; i++){
+		if(artikelNr == artikelTab[i]){
+			return i;
+		}
+	}
+	return -1;
+}
+void Lager::loescheAlleArtikel(){
+	for(int i = 0; i < anzArtikel; i++){
+		delete artikelTab[i];
+	}
+	anzArtikel = 0;
 }
 
--- UEB5/Lager.h
+++ UEB5/Lager.h
@@ -1,30 +1,74 @@
-/*
- * Lager.h
+/**
+ * @file Lager.h
  *
- *  Created on: 23.05.2015
- *      Author: Simon
+ * @date 23.05.2015
+ * @author Andreas Schreiner & Simon Bastian
  */
 
 #ifndef LAGER_H_
 #define LAGER_H_
 #include "Artikel.h"
-
+/**
+ * @brief Eine Lagerklasse
+ */
 class Lager {
 public:
+	/**
+	 * @brief Konstruktor ohne Namensangabe
+	 *
+	 * @param maxAnzArtikel muss positiv sein!
+	 */
 	Lager(int maxAnzArtikel);
+	/**
+	 * @brief Konstruktor mit Namensangabe
+	 * @param maxAnzArtikel muss positiv sein!
+	 * @param name Name des Lagers
+	 */
 	Lager(int maxAnzArtikel, string name);
+	/**
+	 * @brief Destructor löscht alle Artikel
+	 */
 	virtual ~Lager();
+	/**
+	 * @brief Eintragen eines neuen Artikels ohne Bestandsangabe
+	 * @details Artikel wird am Ende ins ArtikelTab eingetragen, falls moeglich
+	 * @param artikelNr
+	 * @param bezeichnung
+	 * @param artikelPreis
+	 */
 	void createArtikel(int artikelNr, string bezeichnung, double artikelPreis);
+	/**
+	 * @brief Eintragen eines neuen Artikels mit Bestandsangabe
+	 * @details Artikel wird am Ende ins ArtikelTab eingetragen, falls moeglich
+	 * @param artikelNr
+	 * @param bezeichnung
+	 * @param artikelPreis
+	 * @param bestand
+	 */
 	void createArtikel(int artikelNr, string bezeichnung, double artikelPreis, int bestand);
+	/**
+	 * @brief Entfernen eines Artikels aus dem Lager
+	 * @param artikelNr
+	 */
 	void deleteArtikel(int artikelNr);
-	void bucheZugang(Artikel artikel);
-	void bucheAbgang(Artikel artikel);
-	void preiseAendern(double prozent);
+	/**
+	 *
+	 */
+	void bucheZugang(Artikel *artikel, int menge);
+	/**
+	 *
+	 */
+	void bucheAbgang(Artikel *artikel, int menge);
+	/**
+	 *
+	 */
+	void preiseAendern(double preisaenderung);
 
 private:
 	int findeArtikel(int artikelNr);
+	void loescheAlleArtikel();
 	string name;
-	Artikel** sortiment;
+	Artikel** artikelTab;
 	int anzArtikel;
 	int maxAnzArtikel;
 };
